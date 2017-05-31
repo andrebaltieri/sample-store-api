4e32ef49b803894c1d5c915c3306f21b4920a40f
Andre Baltieri <andrebaltieri@hotmail.com>
2017-05-31 08:50:09 -0300

    Adicionando o método Seed

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Sample.Api.Models;
+using System;
 
 namespace Sample.Api
 {
@@ -14,15 +16,18 @@ namespace Sample.Api
             services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase());
             services.AddMvc();
         }
-        
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, DataContext context)
         {
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
-            
+
             app.UseMvc();
+
+            context.Customers.Add(new Customer { Id = Guid.NewGuid(), FirstName = "André", LastName = "Baltieri", Document = "99999999999", Email = "hello@balta.io", Birthdate = DateTime.Now.AddYears(-31), Username = "andrebaltieri", Password = "andrebaltieri" });
+            context.SaveChanges();
         }
     }
 }
